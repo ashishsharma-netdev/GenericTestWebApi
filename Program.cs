acb59813e7428068c128b256ee9b678336713de9
ashishsharma-netdev <ashishsharma.netdev@gmail.com>
2026-09-17 18:51:49 +0530

    Register subscription lifecycle services

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using System.Text;
 using GenericTestWebApi.Auth;
 using GenericTestWebApi.Data;
 using GenericTestWebApi.Payments;
+using GenericTestWebApi.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
@@ -11,6 +12,7 @@ builder.Services.AddControllers(); builder.Services.AddEndpointsApiExplorer(); b
 builder.Services.AddDbContext<TestPrepDbContext>(o=>o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.Configure<JwtOptions>(builder.Configuration.GetSection("Jwt")); builder.Services.AddSingleton<JwtTokenService>();
 builder.Services.AddHttpClient(); builder.Services.AddSingleton<RazorpayService>();
+builder.Services.AddScoped<SubscriptionLifecycleService>(); builder.Services.AddHostedService<SubscriptionLifecycleHostedService>();
 var jwt=builder.Configuration.GetSection("Jwt").Get<JwtOptions>() ?? new JwtOptions();
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(o=>{o.TokenValidationParameters=new TokenValidationParameters{ValidateIssuer=true,ValidateAudience=true,ValidateLifetime=true,ValidateIssuerSigningKey=true,ValidIssuer=jwt.Issuer,ValidAudience=jwt.Audience,IssuerSigningKey=new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwt.Key)),ClockSkew=TimeSpan.FromSeconds(30)};});
 builder.Services.AddAuthorization();
