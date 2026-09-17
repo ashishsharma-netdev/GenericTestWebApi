21463a3380c41715da43cba050f6d4ade347c341
ashishsharma-netdev <ashishsharma.netdev@gmail.com>
2026-09-17 17:48:38 +0530

    Register Razorpay services

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using System.Text;
 using GenericTestWebApi.Auth;
 using GenericTestWebApi.Data;
+using GenericTestWebApi.Payments;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
@@ -9,6 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers(); builder.Services.AddEndpointsApiExplorer(); builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<TestPrepDbContext>(o=>o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.Configure<JwtOptions>(builder.Configuration.GetSection("Jwt")); builder.Services.AddSingleton<JwtTokenService>();
+builder.Services.AddHttpClient(); builder.Services.AddSingleton<RazorpayService>();
 var jwt=builder.Configuration.GetSection("Jwt").Get<JwtOptions>() ?? new JwtOptions();
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(o=>{o.TokenValidationParameters=new TokenValidationParameters{ValidateIssuer=true,ValidateAudience=true,ValidateLifetime=true,ValidateIssuerSigningKey=true,ValidIssuer=jwt.Issuer,ValidAudience=jwt.Audience,IssuerSigningKey=new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwt.Key)),ClockSkew=TimeSpan.FromSeconds(30)};});
 builder.Services.AddAuthorization();
